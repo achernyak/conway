28078d2164ac0456475616905576542b76ac87cd
achernyak <artem@ikayzo.com>
2016-05-18 16:50:42 -0500

    naive board

---
+++ vec.clj
@@ -0,0 +1,44 @@
+(defn empty-board
+  "Creates a rectangular empty board of the specified width
+  and height."
+  [w h]
+  (vec (repeat w (vec (repeat h nil)))))
+
+(defn populate
+  "Turns :on each of the cells specified as [y, x] coordinates."
+  [board living-cells]
+  (reduce (fn [board coordinates]
+            (assoc-in board coordinates :on))
+          board
+          living-cells))
+
+(def glider (populate (empty-board 6 6) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
+
+(pprint glider)
+
+(defn neighbours
+  [[x y]]
+  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
+    [(+ dx x) (+ dy y)]))
+(defn count-neighbours
+  [board loc]
+  (count (filter #(get-in board %) (neighbours loc)))) 
+
+(defn indexed-step
+  "Yields the next state of the board, using indices to determine neighbors,
+  liveness, etc."
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (reduce
+     (fn [new-board [x y]]
+       (let [new-liveness
+             (case (count-neighbours board [x y])
+               2 (get-in board [x y])
+               3 :on
+               nil)]
+         (assoc-in new-board [x y] new-liveness)))
+     board (for [x (range h) y (range w)] [x y]))))
+
+
+(-> (iterate indexed-step glider) (nth 8) pprint)
